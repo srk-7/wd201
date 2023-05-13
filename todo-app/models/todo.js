ac566111cdc2fb00a1de9e18eded901010878881
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-05-13 21:27:55 +0530

    tests

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
     static associate(models) {
       // define association here
     }
