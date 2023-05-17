5e685f970fa6566467c0739d0e29c3057413c16c
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2023-05-17 09:52:03 +0530

    Associations: adding owners to todo l10

--- todo-app/models/user.js
+++ todo-app/models/user.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      User.hasMany(models.Todo, {
+        foreignKey: "userId",
+      });
       // define association here
     }
   }
